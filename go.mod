e118ebcde19e993a069575227b0e0e475637d5cf
Oleg Ilyin <ilynrabota@yandex.ru>
2024-05-16 18:40:01 +0500

    welldone

--- go.mod
+++ go.mod
@@ -3,6 +3,12 @@ module github.com/RedrikShuhartRed/go-mongodb-api
 go 1.21.3
 
 require (
-	github.com/julienschmidt/httprouter v1.3.0 // indirect
-	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
+	github.com/globalsign/mgo v0.0.0-20181015135952-eeefdecb41b8
+	github.com/julienschmidt/httprouter v1.3.0
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22
+)
+
+require (
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/RedrikShuhartRed/go-mongodb-api/controllers"
+	"github.com/globalsign/mgo"
 	"github.com/julienschmidt/httprouter"
-	"gopkg.in/mgo.v2"
 )
 
 func getSession() *mgo.Session {
 	s, err := mgo.Dial("mongodb://localhost:27017")
 	if err != nil {
-		panic(err)
+		fmt.Errorf("%w", err)
 	}
+	defer s.Close()
 	return s
 }
 func main() {
@@ -20,7 +22,7 @@ func main() {
 	r := httprouter.New()
 	uc := controllers.NewUserController(getSession())
 	r.GET("/users/:id", uc.GetUser)
-	r.POST("/user", uc.CreatUser)
+	r.POST("/user", uc.CreateUser)
 	r.DELETE("/user/:id", uc.DeleteUser)
 	http.ListenAndServe("localhost:8080", r)
 
